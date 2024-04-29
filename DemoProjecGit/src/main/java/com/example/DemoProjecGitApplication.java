a0a368a5ef10937b0a74fa1f4f17792cf51b8f9b
Rake-7854 <101552087+Rake-7854@users.noreply.github.com>
2024-04-29 13:44:36 +0530

    Remove Remove Remove

--- DemoProjecGit/src/main/java/com/example/DemoProjecGitApplication.java
+++ DemoProjecGit/src/main/java/com/example/DemoProjecGitApplication.java
@@ -8,12 +8,7 @@ public class DemoProjecGitApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hiii This is A First Line Project.....");
-		System.out.println("Hiii This is A First Line Project.....");
-		System.out.println("Hiii This is A First Line Project.....");
-		System.out.println("Hiii This is A First Line Project.....");
-		System.out.println("Hiii This is A First Line Project.....");
-		System.out.println("Hiii This is A First Line Project.....");
-		System.out.println("Hiii This is A First Line Project.....");
+		
 		SpringApplication.run(DemoProjecGitApplication.class, args);
 	}
 
